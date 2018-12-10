29f99a95f6d34957a4679e8e1fb9c9e99cf186cb
Hikmet Baş <hikmet.bas@pardus.org.tr>
2018-12-10 13:28:51 +0300

    Import Upstream version 1.1.0

---
+++ src/settings.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2010 Mike Massonnet, <mmassonnet@xfce.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <glib-object.h>
+
+#define XTM_TYPE_SETTINGS			(xtm_settings_get_type ())
+#define XTM_SETTINGS(obj)			(G_TYPE_CHECK_INSTANCE_CAST ((obj), XTM_TYPE_SETTINGS, XtmSettings))
+#define XTM_SETTINGS_CLASS(klass)		(G_TYPE_CHECK_CLASS_CAST ((klass), XTM_TYPE_SETTINGS, XtmSettingsClass))
+#define XTM_IS_SETTINGS(obj)			(G_TYPE_CHECK_INSTANCE_TYPE ((obj), XTM_TYPE_SETTINGS))
+#define XTM_IS_SETTINGS_CLASS(klass)		(G_TYPE_CHECK_CLASS_TYPE ((klass), XTM_TYPE_SETTINGS))
+#define XTM_SETTINGS_GET_CLASS(obj)		(G_TYPE_INSTANCE_GET_CLASS ((obj), XTM_TYPE_SETTINGS, XtmSettingsClass))
+
+typedef struct _XtmSettings XtmSettings;
+
+GType		xtm_settings_get_type				(void);
+XtmSettings *	xtm_settings_get_default			(void);
+
+
+
+typedef enum
+{
+	XTM_TOOLBAR_STYLE_DEFAULT,
+	XTM_TOOLBAR_STYLE_SMALL,
+	XTM_TOOLBAR_STYLE_LARGE,
+	XTM_TOOLBAR_STYLE_TEXT,
+} XtmToolbarStyle;
+
+GType xtm_toolbar_style_get_type (void);
+#define XTM_TYPE_TOOLBAR_STYLE (xtm_toolbar_style_get_type ())
+
+#endif /* !SETTINGS_H */
